7b64f758218ebed2f4fa3a552e95f40953af0eeb
Samkiroko <kirokodev@gmail.com>
2021-02-28 10:20:46 +0300

    challege

--- .history/script_20210228095604.js
+++ .history/script_20210228095604.js
@@ -1,56 +1,56 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// // Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// // Data needed for first part of the section
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
 
-  //   // order: function (starterIndex, mainIndex) {
-  //   //   return [this.starterMenu[starterIndex]], this.mainMenu[mainIndex];
-  //   // },
+//   // order: function (starterIndex, mainIndex) {
+//   //   return [this.starterMenu[starterIndex]], this.mainMenu[mainIndex];
+//   // },
 
-  //   // oderDelivery: function ({ starterIndex }) {
-  //   //   console.log(obj);
-  //   // },
-  //   // restaurant.oderDelivery({
-  //   //   time: '22:30',
-  //   //   address: 'via del sole, 21',
-  //   //   mainIndex: 2,
-  //   //   starterIndex:2,
-  //   // })
+//   // oderDelivery: function ({ starterIndex }) {
+//   //   console.log(obj);
+//   // },
+//   // restaurant.oderDelivery({
+//   //   time: '22:30',
+//   //   address: 'via del sole, 21',
+//   //   mainIndex: 2,
+//   //   starterIndex:2,
+//   // })
 
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(
-      `Here is your declicious pasts with ${ing1}, ${ing2} and ${ing3}.`
-    );
-  },
-  orderPizza: function (mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-  },
-};
+//   orderPasta: function (ing1, ing2, ing3) {
+//     console.log(
+//       `Here is your declicious pasts with ${ing1}, ${ing2} and ${ing3}.`
+//     );
+//   },
+//   orderPizza: function (mainIngredient, ...otherIngredients) {
+//     console.log(mainIngredient);
+//     console.log(otherIngredients);
+//   },
+// };
 // // use any data type, return any data type short-circuiting
 // console.log(3 || 'jonas');
 // console.log('' || 'jonas');
@@ -228,77 +228,47 @@ Then, call the function again with players from game.scored
 
 */
 
-// const game = {
-//   team1: 'Bayern Munich',
-//   team2: 'Borrussia Dortmund',
-//   players: [
-//     [
-//       'Neuer',
-//       'Pavard',
-//       'Martinez',
-//       'Alaba',
-//       'Davies',
-//       'Kimmich',
-//       'Goretzka',
-//       'Coman',
-//       'Muller',
-//       'Gnarby',
-//       'Lewandowski',
-//     ],
-//     [
-//       'Burki',
-//       'Schulz',
-//       'Hummels',
-//       'Akanji',
-//       'Hakimi',
-//       'Weigl',
-//       'Witsel',
-//       'Hazard',
-//       'Brandt',
-//       'Sancho',
-//       'Gotze',
-//     ],
-//   ],
-//   score: '4:0',
-//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-//   date: 'Nov 9th, 2037',
-//   odds: {
-//     team1: 1.33,
-//     x: 3.25,
-//     team2: 6.5,
-//   },
-// };
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
 
-// //  1
-// const [players1, players2] = game.players;
-// console.log(game.players);
-
-// //2
-// const [gk, ...fieldPlayers] = players1;
-// console.log(gk, fieldPlayers);
-
-// //3
-// const allPlayers = [...players1, ...players2];
-// console.log(allPlayers);
-
-// //4
-// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
-// console.log(players1Final);
-// // 5
-// const {
-//   odds: { team1, x: draw, team2 },
-// } = game;
-// console.log(team1, draw, team2);
-
-// //6
-// const printGoals = function (...players) {
-//   console.log(`${players.length} goals were scored`);
-// };
-
-// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-// printGoals('Davies', 'Muller');
-// printGoals(...game.scored);
-
-// //7
-// team1 > team2 && console.log('Team 1 is more likely to win');
-// team1 < team2 && console.log('Team 2 is more likely to win');
+//  1
+const [players1, players2] = game.players;
+console.log(game.players);
